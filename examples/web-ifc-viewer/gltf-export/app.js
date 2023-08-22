69f8748d08966abc2b403ce380078ba64e2e5259
neostim <beacher@gmail.com>
2023-08-22 11:22:58 -0400

    Update app.js

--- examples/web-ifc-viewer/gltf-export/app.js
+++ examples/web-ifc-viewer/gltf-export/app.js
@@ -38,7 +38,7 @@ async function loadIfc(event) {
             walls: [IFCWALL, IFCWALLSTANDARDCASE],
             slabs: [IFCSLAB],
             windows: [IFCWINDOW],
-            curtainwalls: [IFCBEAM, IFCMEMBER, IFCMEMBERTYPE, IFCPLATE, IFCMECHANICALFASTENER, IFCCURTAINWALL, IFCMATERIAL],
+            curtainwalls: [IFCBEAM, IFCMEMBER, IFCCOLUMN, IFCMEMBERTYPE, IFCPLATE, IFCMECHANICALFASTENER, IFCCURTAINWALL, IFCMATERIAL],
             doors: [IFCDOOR]
         },
         getProperties: false
