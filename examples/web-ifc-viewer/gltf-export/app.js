33d93ddc6087dcffcc2eaab27292307c774dc431
neostim <beacher@gmail.com>
2023-09-06 13:32:30 -0400

    Update app.js
    wth

--- examples/web-ifc-viewer/gltf-export/app.js
+++ examples/web-ifc-viewer/gltf-export/app.js
@@ -16,37 +16,35 @@ import {IFCWALL,
     IFCCOLUMNSTANDARDCASE,    
     IFCCURTAINWALL,
     IFCDOOR} from 'web-ifc';
-
 const container = document.getElementById('viewer-container');
 const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
-//viewer.grid.setGrid();
+viewer.grid.setGrid();
 viewer.axes.setAxes();
 viewer.IFC.setWasmPath("../../../");
-
 const input = document.getElementById('file-input');
 input.onchange = loadIfc;
-
 async function loadIfc(event) {
     const file = event.target.files[0];
     const url = URL.createObjectURL(file);
-
     // Export to glTF and JSON
     const result = await viewer.GLTF.exportIfcFileAsGltf({
         ifcFileUrl: url,
         splitByFloors: false,
         categories: {
-            walls: [IFCWALL, IFCWALLSTANDARDCASE]
+            walls: [IFCWALL, IFCWALLSTANDARDCASE],
+            slabs: [IFCSLAB],
+            windows: [IFCWINDOW],
+            curtainwalls: [IFCBEAM, IFCMEMBER, IFCCOLUMN, IFCMEMBERTYPE, IFCCOLUMNTYPE, IFCCOLUMNSTANDARDCASE, IFCPLATE, IFCMECHANICALFASTENER, IFCCURTAINWALL, IFCMATERIAL],
+            doors: [IFCDOOR]
         },
-        getProperties: true
+        getProperties: false
     });
-
     // Download result
-   // const link = document.createElement('a');
-  //  document.body.appendChild(link);
-
-  //  for(const categoryName in result.gltf) {
-  //      const category = result.gltf[categoryName];
-  //      for(const levelName in category) {
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    for(const categoryName in result.gltf) {
+        const category = result.gltf[categoryName];
+        for(const levelName in category) {
             const file = category[levelName].file;
             if(file) {
                 link.download = `${file.name}_${categoryName}_${levelName}.gltf`;
@@ -55,15 +53,12 @@ async function loadIfc(event) {
             }
         }
     }
-
     for(let jsonFile of result.json) {
         link.download = `${jsonFile.name}.json`;
         link.href = URL.createObjectURL(jsonFile);
         link.click();
     }
-
     link.remove();
 }
-
 window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
