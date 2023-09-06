b98f54861f49c996c1d9dd97c1f37adba187a08e
neostim <beacher@gmail.com>
2023-09-06 13:25:58 -0400

    Update rollup.config.js

--- examples/web-ifc-viewer/gltf-export/rollup.config.js
+++ examples/web-ifc-viewer/gltf-export/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'examples/web-ifc-viewer/gltf-export/app.js',
+  input: 'examples/web-ifc-viewer/gltf-export/app.js?version=f36af92',
   output: [
     {
       format: 'esm',
